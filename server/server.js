080c3242a0d5f7b8b11e8caa1c96246ad431a19f
Quirin Guthmann <quiringuthmann@quikus.de>
2016-08-27 14:05:18 +0200

    gawg

--- server/server.js
+++ server/server.js
@@ -5,8 +5,17 @@
 * only. Define functions in /server/admin/startup-functions.
 */
 
-Meteor.startup(function(){
-
-
-   
+Meteor.startup(function() {
+  if(Books.find().count() === 0) {
+    // Books
+  }
+  if(Categories.find().count() === 0) {
+    // Categories
+    Categories.insert({name:'Tech'});
+    Categories.insert({name:'Design'});
+    Categories.insert({name:'Business'});
+    Categories.insert({name:'Science'});
+    Categories.insert({name:'Scifi'});
+    Categories.insert({name:'Psychology'});
+  }
 });
