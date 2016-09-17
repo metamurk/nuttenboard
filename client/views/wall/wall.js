6722234543741d26129909c6da540dc4af316714
Quirin Guthmann <quiringuthmann@quikus.de>
2016-09-17 16:44:18 +0200

    Auctions can now be added...

--- client/views/wall/wall.js
+++ client/views/wall/wall.js
@@ -1,190 +1,11 @@
 Template.wall.helpers({
   Categories:function() {
-    return [ 
-		{ "_id" : "LbxvvRLi3sxGi8uxd", "name" : "Tech" },
-		{ "_id" : "gsMBngJnmrJaNrkce", "name" : "Design" },
-		{ "_id" : "DddTJYWDWMD9pF83x", "name" : "Business" },
-		{ "_id" : "5H5AtrDogzgRFgbxd", "name" : "Science" },
-		{ "_id" : "xCYzgJivXg3e6aDAr", "name" : "Scifi" },
-		{ "_id" : "KECiZeNcSLoC4jK5g", "name" : "Psychology" }
-		]
+    return Categories.find();
   }
 });
 
 Template.wall.auctions = Template.wall.auctionlist = function(){
-	
-	var url = 'http://youporn.yourporno.biz/files/2012/09/Das-scheint-Porno-G%C3%B6ttin-zu-sein-bei-den-MEGA-Titten.jpg';
-	
-	return [
-	{
-		id : "1",
-		title: "Geile Fotze",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "2",
-		title: "Minipenis",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "3",
-		title: "Wurzelpeter",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "1",
-		title: "Geile Fotze",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "2",
-		title: "Minipenis",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "3",
-		title: "Wurzelpeter",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	},
-	{
-		id : "4",
-		title: "Saufende Wurst",
-		adress: "72001 Siebringen",
-		url: url
-	}
-	]
+	return Auctions.find();;
 };
 
 function masonize(callback) {
@@ -202,3 +23,173 @@ Template.wall.rendered = function() {
     })
   }, 3500);
 };
+
+
+/*{
+		id : "1",
+		title: "Geile Fotze",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "2",
+		title: "Minipenis",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "3",
+		title: "Wurzelpeter",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "1",
+		title: "Geile Fotze",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "2",
+		title: "Minipenis",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "3",
+		title: "Wurzelpeter",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	},
+	{
+		id : "4",
+		title: "Saufende Wurst",
+		adress: "72001 Siebringen",
+		url: url
+	}*/
