5f4117d1aba30d3e88e75a0a8a5e5b1ce25bd366
Quirin Guthmann <quiringuthmann@quikus.de>
2016-10-25 19:05:11 +0200

    Added preview picture for the picture upload...

--- client/views/createAuction/createAuction.js
+++ client/views/createAuction/createAuction.js
@@ -1,59 +1,71 @@
 Meteor.subscribe("auctions");
 
 Template.createAuctionForm.events({
-    'submit': function(){
+    'submit': function () {
         Auctions.insert({
             title: $('#title').val(),
             subtitle: $('#subtitle').val(),
             description: $('#description').val(),
             endDate: new Date()
         });
+    },
+    'change #imgInp': function (input) {
+        input = input.currentTarget;
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-})
+});
 
 Template.uploadForm.onCreated(function () {
-  this.currentUpload = new ReactiveVar(false);
+    this.currentUpload = new ReactiveVar(false);
 });
 
 Template.uploadForm.helpers({
-  currentUpload: function () {
-    return Template.instance().currentUpload.get();
-  }
+    currentUpload: function () {
+        return Template.instance().currentUpload.get();
+    }
 });
 
 Template.uploadForm.events({
-  'change #fileInput': function (e, template) {
-    if (e.currentTarget.files && e.currentTarget.files[0]) {
-      // We upload only one file, in case
-      // multiple files were selected
-      var upload = UserImages.insert({
-        file: e.currentTarget.files[0],
-        streams: 'dynamic',
-        chunkSize: 'dynamic'
-      }, false);
+    'change #fileInput': function (e, template) {
+        if (e.currentTarget.files && e.currentTarget.files[0]) {
+            // We upload only one file, in case
+            // multiple files were selected
+            var upload = UserImages.insert({
+                file: e.currentTarget.files[0],
+                streams: 'dynamic',
+                chunkSize: 'dynamic'
+            }, false);
 
-      upload.on('start', function () {
-        template.currentUpload.set(this);
-      });
+            upload.on('start', function () {
+                template.currentUpload.set(this);
+            });
 
-      upload.on('end', function (error, fileObj) {
-        if (error) {
-          alert('Error during upload: ' + error);
-        } else {
-          alert('File "' + fileObj.name + '" successfully uploaded');
+            upload.on('end', function (error, fileObj) {
+                if (error) {
+                    alert('Error during upload: ' + error);
+                } else {
+                    alert('File "' + fileObj.name + '" successfully uploaded');
+                }
+                template.currentUpload.set(false);
+            });
+
+            upload.start();
         }
-        template.currentUpload.set(false);
-      });
-
-      upload.start();
     }
-  }
 });
 
 Template.file.helpers({
-  imageFile: function () {
-    return UserImages.findOne();
-  }
+    imageFile: function () {
+        return UserImages.findOne();
+    }
 });
 
 
